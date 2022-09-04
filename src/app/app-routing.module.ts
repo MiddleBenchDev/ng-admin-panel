1fbdd661fab0e6dd900842e0d5d3b27507ce0f28
MiddleBenchDev <middlebenchdev@gmail.com>
2022-09-04 13:25:40 +0530

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { QueriesComponent } from './queries/queries.component';
+import { StudentsComponent } from './students/students.component';
+import { TrainersComponent } from './trainers/trainers.component';
+import { SessionsComponent } from './sessions/sessions.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'sessions', component: SessionsComponent },
+  { path: 'trainers', component: TrainersComponent },
+  { path: 'students', component: StudentsComponent },
+  { path: 'queries', component: QueriesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
